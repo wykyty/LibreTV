0f5db744eb99797b754a91a21ae7cfc733cbf8c2
yty <160881580+wykyty@users.noreply.github.com>
2025-12-25 23:41:26 +0800

    Add multiple new content sources to customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -310,6 +310,52 @@ const CUSTOMER_SITES = {
         api: 'https://www.kuaiyunzy.com/api.php/provide/vod/',
         name: '快云|点播',
     },
+    testSource: {
+        api: 'https://www.example.com',
+        name: '空内容测试源',
+        adult: true
+    },
+    ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源',
+        adult: true
+    },
+    jkun: {
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源',
+        adult: true
+    },
+    bwzy: {
+        api: 'https://api.bwzym3u8.com',
+        name: '百万资源',
+        adult: true
+    },
+    souav: {
+        api: 'https://api.souavzy.vip',
+        name: 'souav资源',
+        adult: true
+    },
+    r155: {
+        api: 'https://155api.com',
+        name: '155资源',
+        adult: true
+    },
+    lsb: {
+        api: 'https://apilsbzy1.com',
+        name: 'lsb资源',
+        adult: true
+    },
+    huangcang: {
+        api: 'https://hsckzy.vip',
+        name: '黄色仓库',
+        adult: true,
+        detail: 'https://hsckzy.vip'
+    },
+    yutu: {
+        api: 'https://yutuzy10.com',
+        name: '玉兔资源',
+        adult: true
+    }
 };
 
 // 调用全局方法合并
